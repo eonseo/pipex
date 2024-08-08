b2b215702f5a967d139ab40acf35158508f0f5e8
Eonseo Oh <eonoh@c3r8s2.42gyeongsan.kr>
2024-08-08 22:00:24 +0900

    add: store path

--- srcs/main.c
+++ srcs/main.c
@@ -6,43 +6,41 @@
 /*   By: eonoh <eonoh@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 21:44:20 by eonoh             #+#    #+#             */
-/*   Updated: 2024/08/08 03:34:47 by eonoh            ###   ########.fr       */
+/*   Updated: 2024/08/08 22:00:00 by eonoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-typedef struct s_test
-{
-	char *s;
-	int i;
-} t_test;
-
+#include <stdio.h>
 
 char	*store_path(char *envp[])
 {
 	char	*path;
+	int		i;
 
 	path = NULL;
-	while (*envp)
+	i = 0;
+	while (envp[i])
 	{
-		if (ft_strncmp(*envp, "PATH=", 5) == 0)
-			path = *envp;
-		*envp++;
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			path = envp[i] + 5;
+		i++;
 	}
-	if (path = NULL)
+	if (path == NULL)
 		error("Can't find path in envp.\n");
 	return (path);
 }
 
 int main(int argc, char *argv[], char *envp[])
 {
-	const char	**env = {store_path(envp), NULL};
-	t_test test;
+	//int			pipefd[2];
+	char	*env = store_path(envp);
 
-	test = (t_test){NULL, 0};
-	if (argc != 5)
-		error("Incorrect number of argument.\n");
-	check_argv(argv, envp);
-	pipe_file_io(argv);
+
+	printf("env = %s\n", env);
+	//if (argc != 5)
+	//	error("Incorrect number of argument.\n");
+	//if (pipe(pipefd) == -1)
+	//	error("pipe error\n");
+	//pipe_io(pipefd, env);
 }
